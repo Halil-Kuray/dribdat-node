73d2ffbbd1cce62d5ec42ad63c3acb542610ceec
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-04-11 10:48:27 +0200

    necessary data rendered

--- src/App.js
+++ src/App.js
@@ -22,7 +22,41 @@ function App() {
 
   return (
     <div className="App">
-     
+
+      {eventData && (
+        <section>
+          <h1>{eventData.name}</h1>
+          <img src={eventData.logo_url}/>
+          <img src={eventData.gallery_url}/>
+          <h1> Start :{eventData.starts_at}</h1>
+          <h1> End : {eventData.ends_at}</h1>
+          <h1>Event Details</h1>
+          <p>{eventData.summary}</p>
+          <a href={eventData.webpage_url}> Event Web Page</a>
+          <h1>{eventData.location}</h1>
+        </section>
+      )}
+
+      {projectsData && projectsData.map((projectsData) => (
+        <article key={projectsData.id}>
+          <h1>{projectsData.name}</h1>
+          <a href={projectsData.autotext_url}>Project details</a>
+          <a href={projectsData.contact_url}>contact mail</a>
+          <a href={projectsData.event_url}>Event Url</a>
+          <h3>excerpt</h3>
+          <p>{projectsData.excerpt}</p>
+          <p>{projectsData.summary}</p>
+          <ul>
+            <li> challange created at : {projectsData.created_at} </li>
+            <li> challange updated at : {projectsData.updated_at} </li>
+            <li> challange maintainer : {projectsData.maintainer} </li>
+            <li> challange progress : {projectsData.progress} </li>
+            <li> challange score : {projectsData.score} </li>
+            <li> challange team : {projectsData.team} </li>
+            <li> Team count : {projectsData.team_count} </li>
+          </ul>
+        </article>
+      ))}
     </div>
   );
 }
