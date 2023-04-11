41a2085916899d04e985ad60a5c38e51a8d8be26
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-04-11 10:01:14 +0200

    data fetching process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
 
 function App() {
 
+  const [data, setData] = useState()
+  const [eventData, setEventData] = useState()
+  const [projectsData, setProjectsData] = useState()
+
+  async function fetchData () {
+    const response = await fetch('https://hack.opendata.ch/api/event/current/projects.json')
+    const resData = await response.json()
+    if (resData && resData != null) {
+      setData(resData)
+      setEventData(resData.event)
+      setProjectsData(resData.projects)
+    }
+  }
+
+  useEffect(()=>{
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-     Hello
+     
     </div>
   );
 }
